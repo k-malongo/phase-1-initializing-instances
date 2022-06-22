dfdf50c460f403fc174581cea53d3642ff075cbe
Kelvin <vokemalosh32@gmail.com>
2022-06-22 15:31:13 +0300

    commit tree d6bcb36b8c3ea643b9a0679cfe40989194d30f4c

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code here
+
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+  }
+
+  class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+
+//   Create a class for Dinner. 
+//   Since dinner is a little bit fancier,
+//    Dinner will have a constructor with 
+//    salad, soup, entree, and dessert. 
+//    Initialize dessert as a private property by prefixing 
+//    its name with the hash symbol (#).
+
+  class Dinner {
+    #dessert
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      this.#dessert = dessert
+    }
+  }
+  const dinner = new Dinner()
